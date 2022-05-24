9ca8dcb2d00e5a22b33ee4d9b1aba444fae48eab
ranawp <ranasaha4224#gmail.com>
2022-05-24 11:35:13 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { query } = require('express')
+
+//middelware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xywq3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello world')
 })
+
 
 app.listen(port, () => {
     console.log('starting express', port)
